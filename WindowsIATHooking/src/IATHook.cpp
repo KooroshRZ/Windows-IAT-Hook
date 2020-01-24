fa170735eb3ce421985ad3ab0e4a4126ea94eadf
kooroshrz <knasa.kr@aut.ac.ir>
2020-01-25 00:20:42 +0330

    Simplified the pointers for modifying IAT

--- WindowsIATHooking/src/IATHook.cpp
+++ WindowsIATHooking/src/IATHook.cpp
@@ -5,43 +5,29 @@ BOOL APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
 	if (dwReason == DLL_PROCESS_ATTACH)
 	{
-		//HookFunction((char*)"GetCurrentProcessId", (PSIZE_T)&HookedGetCurrentProcessId);
-		HookFunction((char*)"TerminateProcess", (PSIZE_T)&HookedTerminateProcess);
+		HookFunction((char*)"GetCurrentProcessId", (SIZE_T)HookedGetCurrentProcessId);
+		//HookFunction((char*)"TerminateProcess", (PSIZE_T)&HookedTerminateProcess);
 	}
 	return TRUE;
 }
 
-void HookFunction(char* funcName, PSIZE_T function)
+void HookFunction(char* funcName, SIZE_T function)
 {
 	PSIZE_T pOldFunction = FindFunctionAddress(funcName);
-
 	DWORD accessProtectionValue, accessProtec;
 
 	int vProtect = VirtualProtect(pOldFunction, sizeof(PSIZE_T), PAGE_EXECUTE_READWRITE, &accessProtectionValue);
-
-	//*pOldFunction = (SIZE_T)((PSIZE_T)&HookedGetCurrentProcessId);
-	*pOldFunction = (SIZE_T)function;
-
+	*pOldFunction = function;
 	vProtect = VirtualProtect(pOldFunction, sizeof(PSIZE_T), accessProtectionValue, &accessProtec);
 }
 
-int WINAPI HookedGetCurrentProcessId(VOID)
+DWORD WINAPI HookedGetCurrentProcessId(VOID)
 {
-	//return MessageBoxA(hWnd, "Hello", "DLL answering here!", uType);
 	return 10000;
 }
 
-bool WINAPI
-HookedTerminateProcess(
-	_In_ HANDLE hProcess,
-	_In_ UINT uExitCode
-) {
-	MessageBox(0, "FUCK OFF !!!", "...", MB_ICONINFORMATION);
-	return false;
-}
 
-PSIZE_T FindFunctionAddress(char* funcName)
-{
+PSIZE_T FindFunctionAddress(char* funcName){
 	
 	MODULEINFO modInfo;
 	HMODULE hMod = GetModuleHandle(0);
@@ -65,32 +51,30 @@ PSIZE_T FindFunctionAddress(char* funcName)
 	printf("IMPORT_DIRECTORY_TABLE address %p\n\n", (void*)(pIID));
 	//system("PAUSE");
 
-	for (; pIID->Characteristics; pIID++)
+	for (; pIID->Characteristics; pIID++) 
 		if (!strcmp("KERNEL32.dll", (char*)(lpAddress + pIID->Name))) {
 			printf("IMPORT DIRECROTY ENTRY : %s\n", (char*)(lpAddress + pIID->Name));
 			break;
 		}
 
-
-
 	PIMAGE_THUNK_DATA	pITDN = (PIMAGE_THUNK_DATA)(lpAddress + pIID->OriginalFirstThunk); // ILT
 	PIMAGE_THUNK_DATA	pITDA = (PIMAGE_THUNK_DATA)(lpAddress + pIID->FirstThunk); // IAT
 	PSIZE_T				pOldFunction = nullptr;
 
-
-
 	do {
 		PIMAGE_IMPORT_BY_NAME pIIBN = (PIMAGE_IMPORT_BY_NAME)(lpAddress + pITDN->u1.AddressOfData);
-		if (!strcmp("GetCurrentProcessId", (char*)pIIBN->Name)) {
+
+		if (!strcmp(funcName, (char*)pIIBN->Name)) {
 
 			pOldFunction = (PSIZE_T)&(pITDA->u1.Function);
-			printf("\n\npITDA->u1.Function   :    %p", pITDA->u1.Function);
-			printf("\n	---> %s was found with address : 0x%p\n\n", (char*)pIIBN->Name, *pOldFunction);
+			printf("\n	---> %s was found with address : 0x%p\n\n", (char*)pIIBN->Name, (void*)(*pOldFunction));
 			return pOldFunction;
 		}
+
 		pITDA++;
 		pITDN++;
-	} while (pITDN->u1.AddressOfData);
 
+	} while (pITDN->u1.AddressOfData);
+	
 	return 0;
 }
--- WindowsIATHooking/src/IATHook.h
+++ WindowsIATHooking/src/IATHook.h
@@ -4,19 +4,13 @@
 #include <Psapi.h>
 #include <stdio.h>
 
+
 // Hooked Function Implementations
-int WINAPI HookedGetCurrentProcessId(
+DWORD WINAPI HookedGetCurrentProcessId(
 	VOID
 );
 
-bool WINAPI HookedTerminateProcess(
-	_In_ HANDLE hProcess,
-	_In_ UINT uExitCode
-);
-
-
-
 // Nessesary Functions for Hooking
 PSIZE_T FindFunctionAddress(char* funcName);
-void HookFunction(char* funcName, PSIZE_T function);
+void HookFunction(char* funcName, SIZE_T function);
 
